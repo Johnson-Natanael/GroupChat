54a286278c3aba2c72d47f243f86dc59595766fd
johnson natanael <johnsonnatanael@gmail.com>
2026-06-08 12:59:05 +0700

    basic connection

---
+++ Client/client.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", ":9090")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot connect to server!")
+	} else {
+		fmt.Println("Connected to server!")
+	}
+}
--- Server/server.go
+++ Server/server.go
@@ -4,4 +4,15 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"bufio"
 )
+
+func main() {
+	ln, err := net.Listen("tcp", ":9090")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to listen!")
+		os.Exit(1)
+	} else {
+		fmt.Println("Listening...")
+	}
+}
