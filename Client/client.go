9da1b65e2134fdfdd704b61ac44dcee1b9ddc2ea
johnson natanael <johnsonnatanael@gmail.com>
2026-06-16 01:55:22 +0700

    fitur broadcast to all

--- Client/client.go
+++ Client/client.go
@@ -1,17 +1,41 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"net"
-	"os"
+    "bufio"
+    "fmt"
+    "net"
+    "os"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", ":9090")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Cannot connect to server!")
-	} else {
-		fmt.Println("Connected to server!")
-	}
+    conn, err := net.Dial("tcp", ":9090")
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Tidak dapat terhubung: %v\n", err)
+        os.Exit(1)
+    }
+    defer conn.Close()
+    fmt.Println("Terhubung ke server!")
+
+    // Goroutine: terima pesan dari server
+    go func() {
+        reader := bufio.NewReader(conn)
+        for {
+            line, err := reader.ReadString('\n')
+            if err != nil {
+                fmt.Println("[Koneksi terputus]")
+                os.Exit(0)
+            }
+            fmt.Print(line)
+        }
+    }()
+
+    // Loop utama: kirim pesan ke server
+    reader := bufio.NewReader(os.Stdin)
+    for {
+        text, err := reader.ReadString('\n')
+        if err != nil {
+            break
+        }
+        fmt.Fprintln(conn, text)
+    }
 }
--- Server/server.go
+++ Server/server.go
@@ -1,18 +1,76 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"os"
-	"bufio"
+    "bufio"
+    "fmt"
+    "net"
+    "os"
+    "sync"
 )
 
+type Client struct {
+    conn   net.Conn
+    writer *bufio.Writer
+}
+
+type Server struct {
+    mu      sync.Mutex
+    clients map[net.Conn]*Client
+}
+
+func NewServer() *Server {
+    return &Server{clients: make(map[net.Conn]*Client)}
+}
+
+func (s *Server) broadcast(msg string, exclude net.Conn) {
+    s.mu.Lock()
+    defer s.mu.Unlock()
+    for _, c := range s.clients {
+        if c.conn != exclude {
+            fmt.Fprintln(c.writer, msg)
+            c.writer.Flush()
+        }
+    }
+}
+
+func (s *Server) handleClient(conn net.Conn) {
+    defer conn.Close()
+    writer := bufio.NewWriter(conn)
+    reader := bufio.NewReader(conn)
+
+    s.mu.Lock()
+    s.clients[conn] = &Client{conn: conn, writer: writer}
+    s.mu.Unlock()
+
+    for {
+        line, err := reader.ReadString('\n')
+        if err != nil {
+            break
+        }
+        s.broadcast(line, conn)
+    }
+
+    s.mu.Lock()
+    delete(s.clients, conn)
+    s.mu.Unlock()
+}
+
 func main() {
-	ln, err := net.Listen("tcp", ":9090")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to listen!")
-		os.Exit(1)
-	} else {
-		fmt.Println("Listening...")
-	}
+    ln, err := net.Listen("tcp", ":9090")
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Gagal mendengarkan: %v\n", err)
+        os.Exit(1)
+    }
+    defer ln.Close()
+    fmt.Println("[SERVER] Berjalan di :9090")
+	fmt.Println("[SERVER] Menunggu koneksi...")
+
+    srv := NewServer()
+    for {
+        conn, err := ln.Accept()
+        if err != nil {
+            continue
+        }
+        go srv.handleClient(conn)
+    }
 }
